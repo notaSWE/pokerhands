22eb696657ccad0a3b20da04416c8cad3d431421
NotaSWE <iamnotaswe@gmail.com>
2023-08-15 20:51:50 -0700

    Dealer instance now deals cards from shuffled deck

--- board.py
+++ board.py
@@ -5,39 +5,67 @@ class Board:
   def __init__(self):
     self.display_surface = pygame.display.get_surface()
 
+    self.dealer = Dealer()
+ 
     self.p1 = Player()
     self.p2 = Player()
     self.flop = Flop()
 
-    # Log card data to console
-    print(f"P1: {self.p1.card1.id}, {self.p1.card2.id}")
-    print(f"FL: {self.flop.card1.id}, {self.flop.card2.id}, {self.flop.card3.id}")
-    print(f"P2: {self.p2.card1.id}, {self.p2.card2.id}")
+    # Deal cards and, burn one, turn three
 
-  def draw_cards(self):
+    self.dealer.deal_card(self.p1)
+    self.dealer.deal_card(self.p2)
+    self.dealer.deal_card(self.p1)
+    self.dealer.deal_card(self.p2)
+    self.dealer.deal_card("muck")
+    self.dealer.deal_card(self.flop)
+    self.dealer.deal_card(self.flop)
+    self.dealer.deal_card(self.flop)
+
+    # Log card data to console
+
+    print(f"P1: {[card_id.id for card_id in self.p1.cards]}")
+    print(f"FL: {[card_id.id for card_id in self.flop.cards]}")
+    print(f"P2: {[card_id.id for card_id in self.p2.cards]}")
+
+  def render_cards(self):
     # Player 1 cards on left side of screen
-    card_x = P1_C1[0]  # X coordinate
-    card_y = P1_C1[1] - self.p1.card1.card_surf.get_height() // 2  # Y coordinate adjusted by half of the card's height
-    self.display_surface.blit(self.p1.card1.card_surf, (card_x, card_y))
-    card_x = P1_C2[0]  # X coordinate
-    card_y = P1_C2[1] - self.p1.card2.card_surf.get_height() // 2  # Y coordinate adjusted by half of the card's height
-    self.display_surface.blit(self.p1.card2.card_surf, (card_x, card_y))
+    card_x = P1_C1[0] # X coordinate
+    self.display_surface.blit(self.p1.cards[0].card_surf, (card_x, self.p2.cards[0].card_y))
+    card_x = P1_C2[0] # X coordinate
+    self.display_surface.blit(self.p1.cards[1].card_surf, (card_x, self.p2.cards[1].card_y))
 
     # Player 2 cards on right side of screen
-    card_x = WIDTH - self.p2.card1.card_surf.get_width() - 80
-    card_y = P2_C1[1] - self.p2.card1.card_surf.get_height() // 2  # Y coordinate adjusted by half of the card's height
-    self.display_surface.blit(self.p2.card1.card_surf, (card_x, card_y))
-    card_x = WIDTH - self.p2.card1.card_surf.get_width() - 20
-    card_y = P2_C2[1] - self.p2.card2.card_surf.get_height() // 2  # Y coordinate adjusted by half of the card's height
-    self.display_surface.blit(self.p2.card2.card_surf, (card_x, card_y))
+    card_x = WIDTH - self.p2.cards[0].card_surf.get_width() - 80
+    self.display_surface.blit(self.p2.cards[0].card_surf, (card_x, self.p2.cards[0].card_y))
+    card_x = WIDTH - self.p2.cards[1].card_surf.get_width() - 20
+    self.display_surface.blit(self.p2.cards[1].card_surf, (card_x, self.p2.cards[1].card_y))
 
     # Flop
-    card_x = self.p1.card1.card_surf.get_width() * 2
-    self.display_surface.blit(self.flop.card1.card_surf, (card_x, card_y))
-    card_x += self.p1.card1.card_surf.get_width() + 60
-    self.display_surface.blit(self.flop.card2.card_surf, (card_x, card_y))
-    card_x += self.p1.card1.card_surf.get_width() + 60
-    self.display_surface.blit(self.flop.card3.card_surf, (card_x, card_y))
+    card_x = self.p1.cards[0].card_surf.get_width() * 2
+    self.display_surface.blit(self.flop.cards[0].card_surf, (card_x, self.flop.cards[0].card_y))
+    card_x += self.p1.cards[0].card_surf.get_width() + 60
+    self.display_surface.blit(self.flop.cards[1].card_surf, (card_x, self.flop.cards[1].card_y))
+    card_x += self.p1.cards[0].card_surf.get_width() + 60
+    self.display_surface.blit(self.flop.cards[2].card_surf, (card_x, self.flop.cards[2].card_y))
 
   def update(self):
-    self.draw_cards()
+    self.render_cards()
+
+class Dealer():
+  def __init__(self):
+    self.deck = fresh_deck.copy()
+    random.shuffle(self.deck)
+    self.dealt_cards = []
+
+  def deal_card(self, dest):
+    if dest != "muck":
+      dest.cards.append(self.deck[-1])
+      self.dealt_cards.append(self.deck[-1])
+      self.deck.pop(-1)
+    else:
+      self.dealt_cards.append(self.deck[-1])
+      self.deck.pop(-1)
+    
+    # Print length of deck after card is dealt
+    # print(f"{len(self.deck)} cards left in deck; {len(self.dealt_cards)} dealt.")
--- pokerhands.py
+++ pokerhands.py
@@ -1,7 +1,10 @@
 from collections import namedtuple
+from settings import *
 import pygame, random, time
 
 CardTuple = namedtuple('Card', ['value', 'suit'])
+
+fresh_deck = []
 
 cardvalues = [2, 3, 4, 5, 6, 7, 8, 9, 10,
   11, # Jack
@@ -9,44 +12,27 @@ cardvalues = [2, 3, 4, 5, 6, 7, 8, 9, 10,
   13, # King
   14] # Ace
 
-cardsuits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+cardsuits = ['C', 'D', 'H', 'S']
 
 class Card:
-  def __init__(self):
-    # Does not take into account the "deck" as a whole; collisions are currently possible
-    self.data = CardTuple(value=random.choice(cardvalues), suit=random.choice(cardsuits))
-    self.id = self.shorthand()
+  def __init__(self, input_value, input_suit):
+    self.data = CardTuple(value=input_value, suit=input_suit)
+    self.id = f"{self.data.value}{self.data.suit}"
     self.img = f"graphics/cards/{self.id}.png"
     self.card_img = pygame.image.load(self.img)
     self.card_img = pygame.transform.scale(self.card_img, (self.card_img.get_width() * 4, self.card_img.get_height() * 4))
     self.card_surf = pygame.Surface(self.card_img.get_size())
     self.card_surf.blit(self.card_img, (0, 0))
-
-  def info(self, card):
-    card = card.data
-    if card.value > 10:
-      if card.value == 11:
-        return f"Jack of {card.suit}"
-      elif card.value == 12:
-        return f"Queen of {card.suit}"
-      elif card.value == 13:
-        return f"King of {card.suit}"
-      elif card.value == 14:
-        return f"Ace of {card.suit}"
-    else:
-      return f"{card.value} of {card.suit}"
-
-  def shorthand(self):
-    card = self.data
-    return f"{card.value}{card.suit[0]}"
+    self.card_y = (P1_C1[1] - self.card_surf.get_height() // 2) + random.randint(-20, 20)
 
 class Player:
   def __init__(self):
-    self.card1 = Card()
-    self.card2 = Card()
+    self.cards = []
     
 class Flop:
   def __init__(self):
-    self.card1 = Card()
-    self.card2 = Card()
-    self.card3 = Card()
+    self.cards = []
+
+for cv in cardvalues:
+  for cs in cardsuits:
+    fresh_deck.append(Card(cv, cs))
